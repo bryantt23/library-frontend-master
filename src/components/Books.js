d986d45f15d36d9559a91ec45685613784308380
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-15 00:48:59 -0800

    filter by genre

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { gql, useQuery } from '@apollo/client';
 import { ALL_BOOKS } from '../queries';
 
 const Books = props => {
   const result = useQuery(ALL_BOOKS);
+  // const [genres, setGenres] = useState([]);
+  const [genre, setGenre] = useState('');
+  let genreButtons;
+
+  // useEffect(() => {
+  //   if (genres) {
+  //     createGenreButtons();
+  //   }
+  // }, [genres]);
 
   let books;
   if (result.loading) {
     return <div>loading...</div>;
   } else {
     books = result.data.allBooks;
+    getGenres(books);
     console.log(books);
   }
+
+  //get all genres in a set
+  function getGenres(books) {
+    let genres = new Set();
+    books.forEach(book => {
+      // https://stackoverflow.com/questions/50881453/how-to-add-an-array-of-values-to-a-set
+      genres = new Set([...genres, ...book.genres]);
+    });
+    // setGenres(genres);
+    createGenreButtons(genres);
+  }
+
+  //render genres
+  function createGenreButtons(genres) {
+    // console.log([...genres]);
+    genreButtons = [...genres].map((genre, i) => (
+      <button key={i} onClick={() => setGenre(genre)}>
+        {genre}
+      </button>
+    ));
+  }
+
+  //onclick for each genre
+  //use the click as a state
+  //filter based on that state
 
   if (!props.show) {
     return null;
   }
 
-  //get all genres in a set
-  //render genres
-  //onclick for each genre
-  //use the click as a state
-  //filter based on that state
+  if (genre.length > 0)
+    books = books.filter(book => book.genres.includes(genre));
 
   return (
     <div>
@@ -43,6 +75,7 @@ const Books = props => {
           ))}
         </tbody>
       </table>
+      {genreButtons}
     </div>
   );
 };
